af981335592da00870c30ec558d121f53a03ecd5
Ebuka Abraham <buka4rill@gmail.com>
2020-07-05 20:57:29 +0100

    created default user and roles

--- src/EstateApp.Web/Startup.cs
+++ src/EstateApp.Web/Startup.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Threading.Tasks;
 using EstateApp.Data.DataBaseContexts.ApplicationDbContext;
 using EstateApp.Data.DataBaseContexts.AuthenticationDbContext;
+using EstateApp.Data.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,11 +42,28 @@ namespace EstateApp.Web
                 }
             ));
 
+            // Register Identity
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<AuthenticationDbContext>()
+            .AddDefaultTokenProviders();
+
+            // Control User details
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+            });
+
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider svp)
         {
             if (env.IsDevelopment())
             {
@@ -67,6 +88,65 @@ namespace EstateApp.Web
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            // Run DB Migrations when App starts
+            MigrateDatabaseContexts(svp);
+
+            CreateDefaultRolesAndUsers(svp).GetAwaiter().GetResult(); // Async function
+        }
+
+        public void MigrateDatabaseContexts(IServiceProvider svp)
+        {
+            var authenticationDbContext = svp.GetRequiredService<AuthenticationDbContext>();
+
+            // Compare Migrations with Database and runs migrations
+            authenticationDbContext.Database.Migrate();
+
+            var applicationDbContext = svp.GetRequiredService<ApplicationDbContext>();
+            applicationDbContext.Database.Migrate();
+        }
+
+        // Create Default Users and Default Roles
+        public async Task CreateDefaultRolesAndUsers(IServiceProvider svp)
+        {
+            // Array of roles needed for the App
+            string[] roles = new string[] { "SystemAdministrator", "Agent", "User" };
+
+            // A user manager to manage things related 
+            // To user in the app
+            var userManager = svp.GetRequiredService<UserManager<ApplicationUser>>();
+            var userEmail = "admin@estateapp.com";
+            var userPassword = "SuperSecretPassword@2020";
+
+            // Add User to a role
+            var roleManager = svp.GetRequiredService<RoleManager<IdentityRole>>();
+            foreach (var role in roles)
+            {
+                var roleExists = await roleManager.RoleExistsAsync(role);
+
+                if (!roleExists)
+                {
+                    // Create the role
+                    await roleManager.CreateAsync(new IdentityRole { Name = role });
+                }
+            }
+
+            // Check if the User exist in the DB
+            var user = await userManager.FindByEmailAsync("admin@estateapp.com");
+            if (user is null)
+            {
+                user = new ApplicationUser
+                {
+                    Email = userEmail,
+                    UserName = userEmail,
+                    EmailConfirmed = true,
+                    PhoneNumber = "+234123456789",
+                    PhoneNumberConfirmed = true
+                };
+
+                await userManager.CreateAsync(user, userPassword);
+                await userManager.AddToRolesAsync(user, roles);
+            }
         }
     }
 }
