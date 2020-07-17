1ed92f2b9a1c13323a157e5350b8fa1e35381096
Ebuka Abraham <buka4rill@gmail.com>
2020-07-17 11:11:21 +0100

    Added Login & Register Models

--- src/EstateApp.Web/Controllers/AccountsController.cs
+++ src/EstateApp.Web/Controllers/AccountsController.cs
@@ -1,3 +1,5 @@
+using System;
+using EstateApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EstateApp.Web.Controllers
@@ -6,14 +8,34 @@ namespace EstateApp.Web.Controllers
     {
         // Controller Action for Login Page
         // This one returns the View
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        // Login Action
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid) return View();
+            throw new NotImplementedException();
+        }
+
+        // Controller Action for Register Page
+        // This one returns the View
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        // Register Actions
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid) return View();
+            throw new NotImplementedException();
         }
     }
 }
