bd0da1a6cce55f895eb0de72cf53d4baf1e95283
Ebuka Abraham <buka4rill@gmail.com>
2020-08-01 15:20:13 +0100

    made changes to Layout razor

--- src/EstateApp.Web/Controllers/PropertiesController.cs
+++ src/EstateApp.Web/Controllers/PropertiesController.cs
@@ -1,3 +1,5 @@
+using System;
+using EstateApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EstateApp.Web.Controllers
@@ -6,9 +8,23 @@ namespace EstateApp.Web.Controllers
     {
         // Display list of properties page
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        // Add a property
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(PropertyModel model)
+        {
+            throw new NotImplementedException();
         }
     }
 }
